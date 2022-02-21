41efeb5cc17a7b807bbad2b5e7bf952d94cc0745
Марко Лазић <madskillz.yu@hotmail.com>
2022-02-21 23:12:29 +0100

    [158] - New: Created simple WPF design with 3 buttons and listview

---
+++ WPFTry1/ViewModels/PersonViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFTry1.ViewModels
+{
+    public class PersonViewModel
+    {
+       
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private string adress;
+
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+
+
+
+    }
+}
